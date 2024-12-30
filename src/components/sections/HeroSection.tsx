b10571701df48fb5288e0b51b0f2392b77c439c2
netflypsb <154873617+netflypsb@users.noreply.github.com>
2024-12-30 12:18:59 +0800

    Update HeroSection.tsx

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { ArrowDown } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 export function HeroSection() {
@@ -16,7 +16,7 @@ export function HeroSection() {
         Your Ultimate Image Resizing Tool – Resize your pictures effortlessly for all your favorite platforms in seconds.
       </p>
       <Button onClick={scrollToUpload} size="lg" className="animate-float">
-        Try It Now <ArrowDown className="ml-2" />
+        Create a FREE Account <ArrowRight className="ml-2" />
       </Button>
     </section>
   );
